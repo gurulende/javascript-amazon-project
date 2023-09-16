8f9b545b02c9e0caf344ce35752083d9fd0909ec
gurushant <lgurushant@gmail.com>
2023-09-17 00:24:06 +0530

    save cart in locat storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+cart = [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 },
@@ -7,6 +10,12 @@ export let cart = [{
   quantity: 1
 }];
 
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addToCart(productId){
   let matchingItem;
@@ -25,6 +34,7 @@ export function addToCart(productId){
           quantity: 1
         });
       }
+      saveToStorage();
     }
 
     export function removeFromCart(productId){
@@ -36,5 +46,6 @@ export function addToCart(productId){
         }
       });
       cart = newCart;
+      saveToStorage();
 
     }
